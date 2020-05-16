f423e5d1eb361d162967d83863e0e681f3381776
Ben Hauser <ben@hauser.id>
2020-05-16 23:59:48 +0400

    test: add_pool

---
+++ tests/test_add_pool.py
@@ -0,0 +1,92 @@
+import pytest
+
+import brownie
+
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+
+@pytest.fixture
+def registry_compound(accounts, registry, pool_compound):
+    registry.add_pool(
+        pool_compound,
+        2,
+        [18, 6, 0, 0, 0, 0, 0],
+        b"\x4d\x89\x6d\xbd",
+        {'from': accounts[0]}
+    )
+
+    yield registry
+
+
+def test_add_to_pool_list(registry_compound, pool_compound):
+    assert registry_compound.pool_count() == 1
+    assert registry_compound.pool_list(0) == pool_compound
+
+
+def test_approval(registry_compound, pool_compound, DAI, cDAI):
+    assert DAI.allowance(registry_compound, pool_compound) == 2**256 - 1
+    assert cDAI.allowance(registry_compound, pool_compound) == 2**256 - 1
+
+
+def test_get_pool_coins(registry_compound, pool_compound):
+    coin_info = registry_compound.get_pool_coins(pool_compound)
+
+    assert coin_info['coins'] == [pool_compound.coins(0),  pool_compound.coins(1)] + [ZERO_ADDRESS] * 5
+    assert coin_info['underlying_coins'] == [pool_compound.underlying_coins(0),  pool_compound.underlying_coins(1)] + [ZERO_ADDRESS] * 5
+    assert coin_info['decimals'] == [18, 6, 0, 0, 0, 0, 0]
+
+
+def test_admin_only(accounts, registry, pool_compound):
+    with brownie.reverts("dev: admin-only function"):
+        registry.add_pool(
+            pool_compound,
+            2,
+            [18, 6, 0, 0, 0, 0, 0],
+            b"\x4d\x89\x6d\xbd",
+            {'from': accounts[1]}
+        )
+
+
+def test_cannot_add_twice(accounts, registry_compound, pool_compound):
+    with brownie.reverts("dev: pool exists"):
+        registry_compound.add_pool(
+            pool_compound,
+            2,
+            [18, 6, 0, 0, 0, 0, 0],
+            b"\x4d\x89\x6d\xbd",
+            {'from': accounts[0]}
+        )
+
+
+def test_add_multiple(accounts, registry, pool_y, pool_susd):
+    for pool in (pool_y, pool_susd):
+        registry.add_pool(
+            pool,
+            4,
+            [18, 6, 6, 18, 0, 0, 0],
+            b"\x4d\x89\x6d\xbd",
+            {'from': accounts[0]}
+        )
+
+    assert registry.pool_count() == 2
+    assert registry.pool_list(0) == pool_y
+    assert registry.pool_list(1) == pool_susd
+
+    for pool in [pool_y, pool_susd]:
+        coin_info = registry.get_pool_coins(pool)
+        assert coin_info['decimals'] == [18, 6, 6, 18, 0, 0, 0]
+        for i in range(4):
+            assert coin_info['coins'][i] == pool.coins(i)
+            assert coin_info['underlying_coins'][i] == pool.underlying_coins(i)
+
+
+def test_get_pool_info(accounts, registry, pool_y, pool_susd):
+    empty_pool_info = registry.get_pool_info(pool_y)
+
+    registry.add_pool(pool_y, 4, [1, 2, 3, 4, 0, 0, 0], b"", {'from': accounts[0]})
+    y_pool_info = registry.get_pool_info(pool_y)
+    assert y_pool_info != empty_pool_info
+
+    registry.add_pool(pool_susd, 4, [33, 44, 55, 66, 0, 0, 0], b"", {'from': accounts[0]})
+    susd_pool_info = registry.get_pool_info(pool_susd)
+    assert y_pool_info != susd_pool_info != empty_pool_info
