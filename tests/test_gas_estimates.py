81f82e98fbce0d2deab249a2354a80a7e2bc8eb1
Ben Hauser <ben@hauser.id>
2020-05-23 14:47:48 +0400

    test: add estimator tests

--- tests/test_gas_estimates.py
+++ tests/test_gas_estimates.py
@@ -24,6 +24,26 @@ def test_modify_estimates(accounts, registry, pool_susd, DAI, USDC, sUSD):
         registry.estimate_gas_used(pool_susd, DAI, sUSD)
 
 
+def test_estimator_contract(accounts, registry, pool_susd, DAI, USDC, sUSD):
+    addr = [pool_susd, DAI, USDC, sUSD] + ([ZERO_ADDRESS] * 6)
+    registry.set_gas_estimates(addr, [1, 10, 100, 1000, 0, 0, 0, 0, 0, 0], {'from': accounts[0]})
+
+    estimator = brownie.compile_source("""
+@public
+def estimate_gas_used(_pool: address, _from: address, _to: address) -> uint256:
+    return 31337
+    """).Vyper.deploy({'from': accounts[0]})
+
+
+    assert registry.estimate_gas_used(pool_susd, DAI, USDC) == 111
+
+    registry.set_gas_estimate_contract(pool_susd, estimator, {'from': accounts[0]})
+    assert registry.estimate_gas_used(pool_susd, DAI, USDC) == 31337
+
+    registry.set_gas_estimate_contract(pool_susd, ZERO_ADDRESS, {'from': accounts[0]})
+    assert registry.estimate_gas_used(pool_susd, DAI, USDC) == 111
+
+
 def test_no_value_set(registry, pool_susd, DAI, USDC):
     with brownie.reverts("dev: value not set"):
         registry.estimate_gas_used(pool_susd, DAI, USDC)
@@ -36,3 +56,8 @@ def test_admin_only(accounts, registry):
             [0] * 10,
             {'from': accounts[1]}
         )
+
+
+def test_admin_only_estimator(accounts, registry):
+    with brownie.reverts("dev: admin-only function"):
+        registry.set_gas_estimate_contract(ZERO_ADDRESS, ZERO_ADDRESS, {'from': accounts[1]})
