7127c0701361c9047e88bce61276d1f6d9876f2a
Ben Hauser <ben@hauser.id>
2020-05-26 17:52:27 +0400

    feat: add pad_right utility function

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,14 +8,33 @@ def pack_values(values: List[int]) -> bytes:
     Arguments
     ---------
     values : list
-        List of integer values to pack.
+        List of integer values to pack
 
     Returns
     -------
     bytes
-        32 byte big-endian bytestring of tightly packed values.
+        Bytestring of tightly packed values, right padded right to 32 bytes
     """
 
     packed = b"".join(i.to_bytes(1, "big") for i in values)
     padded = packed + bytes(32 - len(values))
     return padded
+
+
+def right_pad(hexstring: str) -> str:
+    """
+    Right-pad a hex string to 32 bytes.
+
+    Arguments
+    ---------
+    hexstring : str
+        Hex string to be padded
+
+    Returns
+    -------
+    str
+        Hex string right padded to 32 bytes
+    """
+    length = len(hexstring) // 2 - 1
+    pad_amount = 32 - length
+    return f"{hexstring}{'00'*pad_amount}"
