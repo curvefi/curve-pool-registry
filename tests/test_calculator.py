eb62b983b21cd5a22b4e15216ffe1f2c6bf57a7c
Ben Hauser <ben@hauser.id>
2020-06-08 17:33:44 +0400

    test: add calculator tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,9 +8,34 @@ def test_admin_only(accounts, registry):
         registry.set_calculator(ZERO_ADDRESS, ZERO_ADDRESS, {'from': accounts[1]})
 
 
-def test_set_calculator(accounts, registry):
-    assert registry.get_calculator(accounts[1]) == ZERO_ADDRESS
+def test_set_calculator(accounts, registry_compound, pool_compound, calculator):
+    assert registry_compound.get_calculator(pool_compound) == calculator
 
-    registry.set_calculator(accounts[1], accounts[2], {'from': accounts[0]})
+    registry_compound.set_calculator(pool_compound, accounts[2], {'from': accounts[0]})
 
-    assert registry.get_calculator(accounts[1]) == accounts[2]
+    assert registry_compound.get_calculator(pool_compound) == accounts[2]
+
+
+def test_set_does_not_affect_multiple(accounts, registry_all, pool_compound, pool_y, calculator):
+    registry_all.set_calculator(pool_compound, accounts[2], {'from': accounts[0]})
+
+    assert registry_all.get_calculator(pool_compound) == accounts[2]
+    assert registry_all.get_calculator(pool_y) == calculator
+
+
+@pytest.mark.skip
+def test_get_exchange_amounts(accounts, registry_y, pool_y, DAI, TUSD, yDAI, yTUSD):
+    DAI._mint_for_testing(10**24, {'from': accounts[0]})
+    DAI.transfer(pool_y, 10**24, {'from': accounts[0]})
+
+    TUSD._mint_for_testing(10**24, {'from': accounts[0]})
+    TUSD.transfer(pool_y, 10**24, {'from': accounts[0]})
+
+    yDAI._mint_for_testing(10**24, {'from': accounts[0]})
+    yDAI.transfer(pool_y, 10**24, {'from': accounts[0]})
+
+    yTUSD._mint_for_testing(10**24, {'from': accounts[0]})
+    yTUSD.transfer(pool_y, 10**24, {'from': accounts[0]})
+    amounts = [10**8] + [0] * 49#, 20000000, 50000000, 1000000000, 1000000] + [0] * 45
+    registry_y.get_exchange_amounts(pool_y, yDAI, yTUSD, amounts, {'from' : accounts[0]})
+    assert False
---
+++ tests/test_forked.py
@@ -0,0 +1,51 @@
+import pytest
+
+from scripts.utils import pack_values, right_pad
+
+@pytest.fixture(scope="module")
+def pool_renbtc(Contract):
+    yield Contract("0x93054188d876f558f4a66B2EF1d97d16eDf0895B")
+
+
+@pytest.fixture(scope="module")
+def lp_renbtc(Contract):
+    yield Contract("0x49849C98ae39Fff122806C06791Fa73784FB3675")
+
+
+@pytest.fixture(scope="module")
+def RenBTC(Contract):
+    yield Contract("0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D")
+
+
+@pytest.fixture(scope="module")
+def WBTC(Contract):
+    yield Contract("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599")
+
+
+
+@pytest.fixture(scope="module")
+def registry_renbtc(accounts, registry, calculator, pool_renbtc, lp_renbtc):
+    registry.add_pool_without_underlying(
+        pool_renbtc,
+        2,
+        lp_renbtc,
+        calculator,
+        right_pad("0xbd6d894d"),
+        pack_values([8, 8]),
+        pack_values([True] + [False] * 7),
+        {'from': accounts[0]}
+    )
+
+    yield registry
+
+
+def test_get_amounts(registry_renbtc, accounts, pool_renbtc, RenBTC, WBTC):
+    amount = registry_renbtc.get_exchange_amount(pool_renbtc, RenBTC, WBTC, 10**8)
+    tx = registry_renbtc.get_exchange_amounts(
+        pool_renbtc,
+        RenBTC,
+        WBTC,
+        [10**8] + [0]*49,
+        {'from': accounts[0]}
+    )
+    assert amount == amounts[0]
