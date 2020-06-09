9e855b78761ecbd5ae162e12725eec80242ae687
Ben Hauser <ben@hauser.id>
2020-06-09 17:05:14 +0400

    test: update and add test cases for calculator

--- tests/forked/test_amounts_renbtc.py
+++ tests/forked/test_amounts_renbtc.py
@@ -1,9 +1,9 @@
 from brownie.test import given, strategy
-from hypothesis import settings
+from hypothesis import Phase, settings
 
 
-@given(st_amounts=strategy("uint[50]", min_value=10**5, max_value=10**8, unique=True))
-@settings(max_examples=10)
+@given(st_amounts=strategy("uint[100]", min_value=10**5, max_value=10**8, unique=True))
+@settings(max_examples=5, phases=[Phase.generate])
 def test_get_amounts(registry_renbtc, accounts, pool_renbtc, RenBTC, WBTC, st_amounts):
     amounts = registry_renbtc.get_exchange_amounts.call(
         pool_renbtc,
@@ -11,13 +11,13 @@ def test_get_amounts(registry_renbtc, accounts, pool_renbtc, RenBTC, WBTC, st_am
         WBTC,
         st_amounts,
     )
-    for i in range(50):
+    for i in range(100):
         amount = registry_renbtc.get_exchange_amount(pool_renbtc, RenBTC, WBTC, st_amounts[i])
         assert amount == amounts[i]
 
 
-@given(st_amounts=strategy("uint[50]", min_value=10**5, max_value=10**8, unique=True))
-@settings(max_examples=10)
+@given(st_amounts=strategy("uint[100]", min_value=10**5, max_value=10**8, unique=True))
+@settings(max_examples=5, phases=[Phase.generate])
 def test_get_amounts_reversed(registry_renbtc, accounts, pool_renbtc, RenBTC, WBTC, st_amounts):
     amounts = registry_renbtc.get_exchange_amounts.call(
         pool_renbtc,
@@ -25,6 +25,6 @@ def test_get_amounts_reversed(registry_renbtc, accounts, pool_renbtc, RenBTC, WB
         RenBTC,
         st_amounts,
     )
-    for i in range(50):
+    for i in range(100):
         amount = registry_renbtc.get_exchange_amount(pool_renbtc, WBTC, RenBTC, st_amounts[i])
         assert amount == amounts[i]
