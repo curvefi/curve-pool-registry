ae90df496dd72bb9d4107353276a86a86e9185b5
Michael Egorov <michwill@gmail.com>
2020-06-10 13:54:28 +0200

    Deployment script includes calculator

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,24 @@
 import json
 from web3 import middleware
 from web3.gas_strategies.time_based import fast_gas_price_strategy as gas_strategy
-from brownie import web3, accounts, Registry, yERC20, cERC20, Contract
+from brownie import web3, accounts, Registry, yERC20, cERC20, CurveCalc
+# from brownie import Contract
 
 from scripts.utils import pack_values, right_pad
 
-DEPLOYER = "0xC447FcAF1dEf19A583F97b3620627BF69c05b5fB"
-POA = True  # False
+DEPLOYER = accounts[0]  # TEST
+# DEPLOYER = "0xC447FcAF1dEf19A583F97b3620627BF69c05b5fB"
+POA = False
+USE_MIDDLEWARE = False
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 TETHERS = ["0xdAC17F958D2ee523a2206206994597C13D831ec7"] + [ZERO_ADDRESS] * 3
 MAX_COINS = 8
 EMPTY_DECIMALS = pack_values([0] * MAX_COINS)
+
+
+def insert_calculator(params, calculator):
+    return params[:3] + (calculator.address,) + params[3:]
+
 
 POOLS = [
     (  # Compound
@@ -50,35 +58,51 @@ POOLS_NO_UNDERLYING = [
 def main(deployment_address=DEPLOYER):
     deployer = accounts.at(deployment_address) if deployment_address else accounts[1]
 
-    web3.eth.setGasPriceStrategy(gas_strategy)
-    web3.middleware_onion.add(middleware.time_based_cache_middleware)
-    web3.middleware_onion.add(middleware.latest_block_based_cache_middleware)
-    web3.middleware_onion.add(middleware.simple_cache_middleware)
+    if USE_MIDDLEWARE:
+        web3.eth.setGasPriceStrategy(gas_strategy)
+        web3.middleware_onion.add(middleware.time_based_cache_middleware)
+        web3.middleware_onion.add(middleware.latest_block_based_cache_middleware)
+        web3.middleware_onion.add(middleware.simple_cache_middleware)
     if POA:
         web3.middleware_onion.inject(middleware.geth_poa_middleware, layer=0)
 
-    try:
-        registry = Contract("0x2eD8727881A07bB8192C94D1a21ac827d22Fc25C")
-        print("Using saved registry")
-    except Exception:
-        while True:
-            try:
-                registry = Registry.deploy(TETHERS, {'from': deployer})
-            except KeyError:
-                continue
-            break
-        with open('registry.abi', 'w') as f:
-            json.dump(registry.abi, f, indent=True)
+    # try:
+    #     registry = Contract("0x2eD8727881A07bB8192C94D1a21ac827d22Fc25C")
+    #     print("Using saved registry")
+    # except Exception:
+    while True:
+        try:
+            registry = Registry.deploy(TETHERS, {'from': deployer})
+        except KeyError:
+            continue
+        break
+    with open('registry.abi', 'w') as f:
+        json.dump(registry.abi, f, indent=True)
 
-    length = len(POOLS) + len(POOLS_NO_UNDERLYING)
-    for i, pool in enumerate(POOLS+POOLS_NO_UNDERLYING, start=1):
+    while True:
+        try:
+            calculator = CurveCalc.deploy({'from': deployer})
+        except KeyError:
+            continue
+        break
+    with open('calculator.abi', 'w') as f:
+        json.dump(calculator.abi, f, indent=True)
+
+    pools = POOLS
+    pools = [insert_calculator(p, calculator) for p in pools]
+
+    pools_no_underlying = POOLS_NO_UNDERLYING
+    pools_no_underlying = [insert_calculator(p, calculator) for p in pools_no_underlying]
+
+    length = len(pools) + len(pools_no_underlying)
+    for i, pool in enumerate(pools + pools_no_underlying, start=1):
         print(f"Adding pool {i} out of {length}...")
         args = list(pool) + [{'from': deployer}]
         while True:
             try:
-                if pool in POOLS:
+                if pool in pools:
                     registry.add_pool(*args)
-                elif pool in POOLS_NO_UNDERLYING:
+                elif pool in pools_no_underlying:
                     registry.add_pool_without_underlying(*args)
                 else:
                     raise RuntimeError("Something weird happened.")
