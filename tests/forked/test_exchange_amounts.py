ec2c977a1cd14803cfd47ef829de5fdef3f653c5
Ben Hauser <ben@hauser.id>
2020-10-23 02:30:35 +0400

    test: add forked test suite

---
+++ tests/forked/test_exchange_amounts.py
@@ -0,0 +1,39 @@
+import pytest
+from brownie.test import given, strategy
+from hypothesis import Phase, settings
+
+
+@pytest.mark.itercoins("send", "recv")
+@given(st_amounts=strategy("decimal[100]", min_value="0.05", max_value=10, places=2, unique=True))
+@settings(max_examples=3, phases=[Phase.generate])
+def test_get_amounts_wrapped(registry_swap, swap, wrapped_coins, wrapped_decimals, send, recv, st_amounts):
+    base_amount = 10**wrapped_decimals[send]
+    st_amounts = [int(base_amount * i) for i in st_amounts]
+
+    send = wrapped_coins[send]
+    recv = wrapped_coins[recv]
+
+    amounts = registry_swap.get_exchange_amounts(swap, send, recv, st_amounts)
+
+    for i in range(100):
+        # `get_exchange_amount` is a thin wrapper that calls `swap.get_dy`
+        amount = registry_swap.get_exchange_amount(swap, send, recv, st_amounts[i])
+        assert amount == amounts[i]
+
+
+@pytest.mark.itercoins("send", "recv", underlying=True)
+@given(st_amounts=strategy("decimal[100]", min_value="0.05", max_value=10, places=2, unique=True))
+@settings(max_examples=3, phases=[Phase.generate])
+def test_get_amounts_underlying(registry_swap, swap, underlying_coins, underlying_decimals, send, recv, st_amounts):
+    base_amount = 10**underlying_decimals[send]
+    st_amounts = [int(base_amount * i) for i in st_amounts]
+
+    send = underlying_coins[send]
+    recv = underlying_coins[recv]
+
+    amounts = registry_swap.get_exchange_amounts(swap, send, recv, st_amounts)
+
+    for i in range(100):
+        # `get_exchange_amount` is a thin wrapper that calls `swap.get_dy_underlying`
+        amount = registry_swap.get_exchange_amount(swap, send, recv, st_amounts[i])
+        assert amount == amounts[i]
---
+++ tests/forked/test_input_amount.py
@@ -0,0 +1,29 @@
+import pytest
+from brownie.test import given, strategy
+from hypothesis import Phase, settings
+
+
+@pytest.mark.itercoins("send", "recv", underlying=True)
+@given(st_amount=strategy("decimal", min_value="0.5", max_value=1000, places=2))
+@settings(max_examples=50, phases=[Phase.generate])
+def test_get_input_amount(registry_swap, swap, underlying_coins, underlying_decimals, send, recv, st_amount):
+    dy = int(10**underlying_decimals[recv] * st_amount)
+
+    send = underlying_coins[send]
+    recv = underlying_coins[recv]
+
+    dx = registry_swap.get_input_amount(swap, send, recv, dy)
+    assert registry_swap.get_exchange_amount(swap, send, recv, dx) == dy
+
+
+@pytest.mark.itercoins("send", "recv")
+@given(st_amount=strategy("decimal", min_value="0.5", max_value=1000, places=2))
+@settings(max_examples=50, phases=[Phase.generate])
+def test_get_input_amount_wrapped(registry_swap, swap, wrapped_coins, wrapped_decimals, send, recv, st_amount):
+    dy = int(10 ** wrapped_decimals[recv] * st_amount)
+
+    send = wrapped_coins[send]
+    recv = wrapped_coins[recv]
+
+    dx = registry_swap.get_input_amount(swap, send, recv, dy)
+    assert registry_swap.get_exchange_amount(swap, send, recv, dx) == dy
