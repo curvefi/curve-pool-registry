32c7d4bb8301c8e75c0d2efcdf8b3147227295ce
Ben Hauser <ben@hauser.id>
2020-10-30 01:04:21 +0400

    feat: deployment script for main registry

---
+++ scripts/deploy.py
@@ -0,0 +1,29 @@
+from brownie import AddressProvider, Registry, accounts
+
+from scripts.add_pools import main as add_pools
+from scripts.utils import get_gas_price
+
+# modify this prior to mainnet use
+deployer = accounts.at("0x7EeAC6CDdbd1D0B8aF061742D41877D7F707289a", force=True)
+
+ADDRESS_PROVIDER = "0x0000000022D53366457F9d5E68Ec105046FC4383"
+GAUGE_CONTROLLER = "0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB"
+
+
+def deploy_registry():
+    """
+    Deploy `Registry`, add all current pools, and set the address in `AddressProvider`.
+    """
+    balance = deployer.balance()
+
+    provider = AddressProvider.at(ADDRESS_PROVIDER)
+    registry = Registry.deploy(
+        ADDRESS_PROVIDER,
+        GAUGE_CONTROLLER,
+        {'from': deployer, 'gas_price': get_gas_price()}
+    )
+    add_pools(registry, deployer)
+    provider.set_address(0, registry, {'from': deployer, 'gas_price': get_gas_price()})
+
+    print(f"Registry deployed to: {registry.address}")
+    print(f"Total gas used: {(balance - deployer.balance()) / 1e18:.4f} eth")
