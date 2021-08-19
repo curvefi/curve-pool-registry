6fdd93665fae4231e71e4d4ae4aeb8c26c15513d
Edward Amor <edward.amor3@gmail.com>
2021-08-19 05:51:54 -0400

    test: admin registering a gauge

--- tests/local/unitary/GaugeRegistry/test_register.py
+++ tests/local/unitary/GaugeRegistry/test_register.py
@@ -8,3 +8,12 @@ def test_newly_deployed_gauge_auto_registers(alice, gauge_registry, mock_factory
     assert gauge_registry.gauge_list(0) == tx.new_contracts[0]
     assert gauge_registry.gauge_version(tx.new_contracts[0]) == 1
     assert gauge_registry.pool_address(tx.new_contracts[0]) == ETH_ADDRESS
+
+
+def test_admin_can_register_a_gauge(alice, gauge_registry):
+    gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42)
+
+    assert gauge_registry.gauge_count() == 1
+    assert gauge_registry.gauge_list(0) == ETH_ADDRESS
+    assert gauge_registry.gauge_version(ETH_ADDRESS) == 42
+    assert gauge_registry.pool_address(ETH_ADDRESS) == ETH_ADDRESS
