1d8f4b9703b79c322eb588288e2dd98019c8b33c
Edward Amor <edward.amor3@gmail.com>
2021-08-19 15:42:44 -0400

    test: update gauge registry register tests
    assert last_updated time updates

--- tests/local/unitary/GaugeRegistry/test_register.py
+++ tests/local/unitary/GaugeRegistry/test_register.py
@@ -9,17 +9,21 @@ def test_newly_deployed_gauge_auto_registers(alice, gauge_registry, mock_factory
     assert gauge_registry.gauge_list(0) == tx.new_contracts[0]
     assert gauge_registry.gauge_version(tx.new_contracts[0]) == 1
     assert gauge_registry.pool_address(tx.new_contracts[0]) == ETH_ADDRESS
+    assert gauge_registry.last_updated() == tx.timestamp
 
 
 def test_admin_can_register_a_gauge(alice, gauge_registry):
-    gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42, {"from": alice})
+    tx = gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42, {"from": alice})
 
     assert gauge_registry.gauge_count() == 1
     assert gauge_registry.gauge_list(0) == ETH_ADDRESS
     assert gauge_registry.gauge_version(ETH_ADDRESS) == 42
     assert gauge_registry.pool_address(ETH_ADDRESS) == ETH_ADDRESS
+    assert gauge_registry.last_updated() == tx.timestamp
 
 
 def test_guarded_register(bob, gauge_registry):
     with brownie.reverts():
         gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42, {"from": bob})
+
+    assert gauge_registry.last_updated() == 0
