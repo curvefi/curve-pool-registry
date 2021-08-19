d63651ade7ce465a24c6e69fd1b2ab485cfcfb36
Edward Amor <edward.amor3@gmail.com>
2021-08-19 05:53:30 -0400

    test: registering is gaurded

--- tests/local/unitary/GaugeRegistry/test_register.py
+++ tests/local/unitary/GaugeRegistry/test_register.py
@@ -1,3 +1,4 @@
+import brownie
 from brownie import ETH_ADDRESS
 
 
@@ -11,9 +12,14 @@ def test_newly_deployed_gauge_auto_registers(alice, gauge_registry, mock_factory
 
 
 def test_admin_can_register_a_gauge(alice, gauge_registry):
-    gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42)
+    gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42, {"from": alice})
 
     assert gauge_registry.gauge_count() == 1
     assert gauge_registry.gauge_list(0) == ETH_ADDRESS
     assert gauge_registry.gauge_version(ETH_ADDRESS) == 42
     assert gauge_registry.pool_address(ETH_ADDRESS) == ETH_ADDRESS
+
+
+def test_guarded_register(bob, gauge_registry):
+    with brownie.reverts():
+        gauge_registry.register(ETH_ADDRESS, ETH_ADDRESS, 42, {"from": bob})
